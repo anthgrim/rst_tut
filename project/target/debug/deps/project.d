bd5f8823fe48d522dc90953301c470aa19c06f43
Kevin A. Grimaldi <85716550+anthgrim@users.noreply.github.com>
2023-06-12 18:24:42 -0400

    Delete project/target directory

--- project/target/debug/deps/project.d
+++
@@ -1,5 +0,0 @@
-C:\Users\anthg\Documents\rust\rst_tut\project\target\debug\deps\project.exe: src\main.rs
-
-C:\Users\anthg\Documents\rust\rst_tut\project\target\debug\deps\project.d: src\main.rs
-
-src\main.rs:
--- project/target/debug/project.d
+++
@@ -1,1 +0,0 @@
-C:\Users\anthg\Documents\rust\rst_tut\project\target\debug\project.exe: C:\Users\anthg\Documents\rust\rst_tut\project\src\main.rs
